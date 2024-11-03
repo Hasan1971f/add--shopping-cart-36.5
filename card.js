3dc00d26309f74edaa4c08cb6cdfa3ef495679bc
Hasan <mdhasanurrahmanhasan101@gmail.com>
2024-11-03 21:45:02 +0600

    added fullfiled completed LocalStorage

--- card.js
+++ card.js
@@ -1,46 +1,53 @@
-// const addProduct=()=>{
-//     const productField = document.getElementById('product-name')
-//     const quantitytField = document.getElementById('product-quantity')
-
-//     const product = productField.value
-//     const quantity = quantitytField.value
-
-//     productField.value= ""
-//     quantitytField.value= ""
-    
-    
-//     console.log(product, quantity)
-//     displayProduct(product, quantity ) 
-// }
-
-// const displayProduct = (product, quantity)=>{
-//     const ul = document.getElementById("product-container")
-//     const li = document.createElement('li')
-//     li.innerHTML= `${product}: ${quantity}`
-//     ul.appendChild(li)
-
-// }
-
-
 const addProduct=()=>{
-    const productField = document.getElementById("product-name")
-    const quantitytField = document.getElementById("product-quantity")
+    const productField = document.getElementById('product-name')
+    const quantitytField = document.getElementById('product-quantity')
 
-    const product1 = productField.value
-    const quantity1 = quantitytField.value
-    
+    const product = productField.value
+    const quantity = quantitytField.value
+
     productField.value= ""
     quantitytField.value= ""
+    
+    
+    console.log(product, quantity)
+    displayProduct(product, quantity )
+    
+    saveProductToLocalStorage(product, quantity )
+}
 
+const displayProduct = (product, quantity)=>{
+    const ul = document.getElementById("product-container")
+    const li = document.createElement('li')
+    li.innerHTML= `${product}: ${quantity}`
+    ul.appendChild(li)
 
+}
 
-    console.log(product1, quantity1)
-    displayProduct(product1, quantity1)
- }
+const getStoredShoppingCart=()=>{
+    let cart = {}
+    const storedCart = localStorage.getItem('cart')
+    
+    if(storedCart){
+       cart = JSON.parse(storedCart)
+    }
+    return cart
+}
 
- const displayProduct=(product1 , quantity1 )=>{
-  const ul = document.getElementById("product-container")
-  const li = document.createElement("li")
-  li.innerHTML= `${product1} : ${quantity1}`
-  ul.appendChild(li)
- }
+const saveProductToLocalStorage =(product, quantity)=>{
+  const cart = getStoredShoppingCart()
+  cart [product] = quantity
+  const cartStrinfy = JSON.stringify(cart)
+  localStorage.setItem('cart', cartStrinfy)
+}
+
+const displayProductFromLocalStorage = ()=>{
+    const saveCart =  getStoredShoppingCart()
+    console.log(saveCart)
+    for(const product in saveCart){
+        const quantity = saveCart[product]
+        
+        console.log(product, quantity)
+        displayProduct (product, quantity)
+    }
+}
+displayProductFromLocalStorage ()
